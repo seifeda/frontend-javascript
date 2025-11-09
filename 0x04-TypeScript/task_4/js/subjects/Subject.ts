7ea26011b3156fa8b62ad17a66a7584499c714c6
Seifeda <seifebekele07@gmail.com>
2025-11-09 14:24:16 +0300

    Adding task 4 subject.ts

--- 0x04-TypeScript/task_4/js/subjects/Subject.ts
+++ 0x04-TypeScript/task_4/js/subjects/Subject.ts
@@ -2,6 +2,7 @@ namespace Subjects {
   export class Subject {
     teacher: Teacher | null = null;
 
+    // Setter method to assign a teacher
     setTeacher(teacher: Teacher) {
       this.teacher = teacher;
     }
